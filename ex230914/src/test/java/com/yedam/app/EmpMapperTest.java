1ff3fa6341c98349f2fdad1fac2e51ab4671ead7
eocjf324 <57933747+eocjf324@users.noreply.github.com>
2023-09-14 17:52:24 +0900

    spring

---
+++ ex230914/src/test/java/com/yedam/app/EmpMapperTest.java
@@ -0,0 +1,57 @@
+package com.yedam.app;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.yedam.app.emp.mapper.EmpMapper;
+import com.yedam.app.emp.service.EmpVO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/spring/root-context.xml")
+public class EmpMapperTest {
+	
+	@Autowired
+	EmpMapper empMapper;
+	
+	
+	@Test
+	public void selectAllEmp() {
+		//전체조회
+		List<EmpVO> empList = empMapper.seleletEmplAllList(null);
+		assertNotNull(empList);
+		
+	}
+	
+	@Test
+	public void selectEmpInfo() {
+		//단건조회
+		EmpVO empVO = new EmpVO();
+		empVO.setEmployeeId(100);
+		
+		EmpVO findVO = empMapper.selectEmpInfo(empVO);
+		assertEquals(findVO.getLastName(), "King");;
+	}
+	
+	@Test
+	public void insertEmpInfo() {
+		//등록
+		EmpVO empVO = new EmpVO();
+		empVO.setLastName("kang");
+		empVO.setFirstName("Na-La");
+		empVO.setEmail("nlKang");
+		empVO.setJobId("IT_PROG");
+		
+		empMapper.insertEmpInfo(empVO);
+		assertNotEquals(empVO.getEmployeeId(), 0); //이거머냐,,
+ 		
+	}
+}
