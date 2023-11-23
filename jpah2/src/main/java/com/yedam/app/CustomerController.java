bac99302749bc06f36a9649dbef887b2fd25e139
dc <tlseocjf324@naver.com>
2023-11-23 10:21:40 +0900

    ddd

--- jpah2/src/main/java/com/yedam/app/CustomerController.java
+++ jpah2/src/main/java/com/yedam/app/CustomerController.java
@@ -1,10 +1,9 @@
 package com.yedam.app;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.yedam.app.accessingdatajpa.Customer;
 import com.yedam.app.accessingdatajpa.CustomerRepository;
@@ -12,7 +11,15 @@ import com.yedam.app.accessingdatajpa.CustomerRepository;
 @RestController  //Controller , ResponseBody
 public class CustomerController {
 
-	@Autowired CustomerRepository repo;
+	@Autowired 
+	CustomerRepository repo;
+	
+	@GetMapping("/")
+	public ModelAndView main() {
+		ModelAndView mv = new ModelAndView("home");
+		mv.addObject("greet", "<b>안녕</b>하세요");
+		return mv;
+	}
 	
 	@GetMapping("/customer")
 	public Iterable<Customer> findall(){
